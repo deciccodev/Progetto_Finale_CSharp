c9817a9b33e1a5e930ec54cdb2bafb609300f915
Nambon03 <deciccodev@gmail.com>
2026-03-23 11:06:25 +0100

    script istanziazione bottoni
    Update:
    + aggiunto script che istanzia i bottoni per i quiz a scelta multipla

--- Assets/Scripts/Marco/IstanziaBottoniQuizSceltaMultipla.cs
+++ Assets/Scripts/Marco/IstanziaBottoniQuizSceltaMultipla.cs
@@ -1,16 +1,53 @@
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class IstanziaBottoniQuizSceltaMultipla : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Header("Setup")]
+    public GameObject bottonePrefab;
+    public Transform content; // Content della Scroll View
+
+    void Start() // START PRESENTE SOLO PER TESTARE LO SCRIPT, DA RIMUOVERE QUANDO SI COLLEGA IL JSON
     {
-        
+        string[] opzioniTest = new string[]
+        {
+            "Risposta 1",
+            "Risposta 2",
+            "Risposta 3",
+            "Risposta 4",
+        };
+
+        CreaBottoni(opzioniTest);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void CreaBottoni(string[] opzioni) // Metodo da chiamare passando le opzioni della domanda
     {
-        
+        // Pulisce i bottoni esistenti
+        for (int i = content.childCount - 1; i >= 0; i--)
+        {
+            Destroy(content.GetChild(i).gameObject);
+        }
+
+        // Istanzia i nuovi bottoni
+        foreach (string opzione in opzioni)
+        {
+            GameObject btn = Instantiate(bottonePrefab, content);
+
+            TMP_Text testo = btn.GetComponentInChildren<TMP_Text>();
+            if (testo != null)
+            {
+                testo.text = opzione;
+            }
+            else
+            {
+                Text testoUI = btn.GetComponentInChildren<Text>();
+                if (testoUI != null)
+                    testoUI.text = opzione;
+            }
+        }
+
+        // Forza aggiornamento layout (IMPORTANTE per Scroll View)
+        LayoutRebuilder.ForceRebuildLayoutImmediate(content.GetComponent<RectTransform>());
     }
 }
