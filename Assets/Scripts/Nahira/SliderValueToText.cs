e2e6d90a5713153adfde53a9ddd7f09b53df2043
Nambon03 <deciccodev@gmail.com>
2026-03-25 17:50:55 +0100

    script aggiunto allo slider del volume

--- Assets/Scripts/Nahira/SliderValueToText.cs
+++ Assets/Scripts/Nahira/SliderValueToText.cs
@@ -4,21 +4,20 @@ using TMPro;
 public class SliderValueToText : MonoBehaviour
 {
     [Header("Riferimenti UI")]
-    public Slider mioSlider;
-    public TextMeshProUGUI testoValore;
+    [SerializeField] Slider mioSlider;
+    [SerializeField] TextMeshProUGUI testoValore;
+    
     [Header("Impostazioni")]
-    public string prefisso = "Valore: ";
-    public string suffisso = "%";
+    [SerializeField] string suffisso = "%";
     void Start()
     {
         if (mioSlider == null) mioSlider = GetComponent<Slider>();
-        // imposto il testo iniziale all'avvio
         AggiornaTesto(mioSlider.value);
-        // se non esiste già aggiungo un "ascoltatore" che chiama la funzione ogni volta che muovi lo slider
-        // mioSlider.onValueChanged.AddListener(delegate { AggiornaTesto(mioSlider.value); });
+
+        mioSlider.onValueChanged.AddListener(delegate { AggiornaTesto(mioSlider.value); });
     }
     public void AggiornaTesto(float valore)
     {
-        testoValore.text = prefisso + valore.ToString("F0") + suffisso;
+        testoValore.text = valore.ToString("F0") + suffisso;
     }
 }
